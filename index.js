d225510afbeadc42beb4030511dfe08a4b996c70
Tom Wilson <twilson63@gmail.com>
2021-02-04 17:57:55 -0500

    updated index js

--- index.js
+++ index.js
@@ -8,33 +8,16 @@ const Movie = (title='', year='1980', genres=[]) =>
   ({ id: `${toLower(title)}-${year}`, type: 'movie', title, year, genres })
 
 // Ghostbusters, 1984, [action, comedy]
+const ghostbusters = Movie('Ghostbusters', '1984', ['action', 'comedy'])
 // Groundhog Day, 1993, [comedy, fantasy, romance]
+const groundhogday = Movie('Groundhog-Day', '1993', ['comedy', 'fantasy', 'romance'])
 // Avengers, 2012, [action, adventure, sci-fi]
+const avengers = Movie('Avengers', '2012', ['action', 'adventure', 'sci-fi'])
 
 const $ = request(url, token)
 async function main() {
   let res = 'Welcome to the hyper63 tour!'
-  const app = 'twilson63-movies'
-  /*
-  res = await $.put(`/search/${app}`, { 
-    fields: ['title', 'year'],
-    storeFields: ['id', 'type']
-  })
-  */
-  /*
-  res = await $.post(`/search/${app}/_bulk`, [Movie('GroundhogDay', '1993', []), 
-    Movie('Avengers', '2012', [])
-  ])
-  */
-  /*
-  res = await $.post(`/search/${app}`, {
-    key: 'ghostbusters-1984', 
-    doc: Movie('Ghostbusters', '1984', [])
-  })
-  */
-  res = await $.post(`/search/${app}/_query`, {
-    query: 'Avengers'
-  })
+  const app = 'YOURNAME-movies'
 
   console.log(JSON.stringify(res))
 
